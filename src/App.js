311b19ee6c967d73955635896330b16dac3ce5d3
RileyMPotter <rileymatpotter@gmail.com>
2023-11-01 22:24:49 -0400

    done lab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-     <h1>CS 230L</h1>
+      
+  <NavBar />
+  <h1>CS 230L</h1>
      <h2>Section - 03</h2>
      <p>WVU ID: 800405794</p>
      <p>Hi I am Riley Potter</p>
+< Card />
+  
+
     </div>
   );
 }
